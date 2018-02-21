a6e3f82bbac1d92768c382190179017b767c5d64
Eduardo Capela <edufeq03@gmail.com>
2018-02-21 18:34:19 -0300

    Ajdust Models/Many Through

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -3,6 +3,15 @@ class Sell < ApplicationRecord
   enum status: { finished: 0, canceled: 1 }
   validates :client, presence: true
 
+  belongs_to :discount
+  belongs_to :client
+
+  has_many :sell_products
+  has_many :products, through: :sell_products
+
+  has_many :sell_services
+  has_many :services, through: :sell_services
+
   def fae_display_field
     id
   end
@@ -10,7 +19,4 @@ class Sell < ApplicationRecord
   def self.for_fae_index
     order(:id)
   end
-
-  belongs_to :discount
-  belongs_to :client
 end
